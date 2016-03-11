e570afd2e429209cf8a1aac8c9532fa77c666bd1
GregBestland <Greg.Bestland@datastax.com>
2016-03-11 17:07:00 -0600

    updating yml for results uploading

--- run_test.ps1
+++ run_test.ps1
@@ -1,2 +1,4 @@
 nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=unit_results.xml .\tests\unit
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\unit_results.xml))
 exit 0

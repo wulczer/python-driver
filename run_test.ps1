acfeb546c72860cb1cc4345b5d09ce26239ee67f
GregBestland <Greg.Bestland@datastax.com>
2016-04-28 16:02:41 -0500

    debug integration tests

--- run_test.ps1
+++ run_test.ps1
@@ -1,8 +1,6 @@
 nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=unit_results.xml .\tests\unit
-nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard
 echo "uploading results"
 echo $env:APPVEYOR_JOB_ID
 $wc = New-Object 'System.Net.WebClient'
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\unit_results.xml))
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\standard_results.xml))
 exit 0
